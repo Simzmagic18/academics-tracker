6607d9bcdea1dde7d5940e446b6d528e73fd2ad3
David <dchimzizi@gmail.com>
2018-10-03 03:26:40 +0200

    EDITING THE ADD ADMIN

--- Admin/RegisterNewAdministrator.php
+++ Admin/RegisterNewAdministrator.php
@@ -10,7 +10,7 @@ if($link === false)
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-	$sql1 = "INSERT INTO administrator (admin_first_name, admin_middle_name, admin_last_name, date_of_birth, ethnicity, gender, id, school_code, password, user_type) VALUES ('$_POST[first_name]','$_POST[middle_name]','$_POST[last_name]','$_POST[date_of_birth]','$_POST[ethnicity]','$_POST[gender]','$_POST[id]','$_POST[school_code]','$_POST[password]','$_POST[user_type]')";
+	$sql1 = "INSERT INTO administrator (admin_first_name, admin_middle_name, admin_last_name, date_of_birth, ethnicity, gender, id, school_code, password, user_type) VALUES ('$_POST[admin_first_name]','$_POST[admin_middle_name]','$_POST[admin_last_name]','$_POST[date_of_birth]','$_POST[ethnicity]','$_POST[gender]','$_POST[id]','$_POST[school_code]','$_POST[password]','$_POST[user_type]')";
 	//$sql2 = "INSERT INTO contacts (house_number, street_name, suburb, post_code, contact_number, id, user_type) VALUES ('$_POST[house_number]','$_POST[street_name]','$_POST[suburb]','$_POST[post_code]','$_POST[contact_number]','$_POST[id]','$_POST[user_type]')";
 
 if(mysqli_query($link, $sql1))
