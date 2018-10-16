908e3ae75d360ea36f48f7a721e9bae422e1834e
Simbarashe Sibusiso Sithole <simzmagic8@gmail.com>
2018-10-16 14:05:30 +0200

    Add teacher

--- Admin/RegisterNewTeacher.php
+++ Admin/RegisterNewTeacher.php
@@ -3,22 +3,62 @@
 
 <?php
 
-$link = mysqli_connect("localhost", "root", "", "academicsTracker2");
+$con = mysqli_connect("localhost", "root", "", "academicsTracker2");
  
-if($link === false)
+if($con === false)
 {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+
+
+
+/*$hashed_password = crypt('password'); // let the salt be automatically generated
+
+/* You should pass the entire results of crypt() as the salt for comparing a
+   password, to avoid problems when different hashing algorithms are used. (As
+   it says above, standard DES-based password hashing uses a 2-character salt,
+   but MD5-based hashing uses 12.) 
+if (hash_equals($hashed_password, crypt( $hashed_password))) {
+   echo "Password verified!";
+} */
+
+
+
 
 //$password1= hash('sha512', '$_POST[password]');
 
 //$sql = "INSERT INTO teacher(password) VALUES ($password1)";
 	//
-	
-$sql1 = "INSERT INTO teacher (teacher_first_name, teacher_middle_name, teacher_last_name, date_of_birth, ethnicity, gender, password, user_type) VALUES ('$_POST[teacher_first_name]','$_POST[teacher_middle_name]','$_POST[teacher_last_name]','$_POST[date_of_birth]','$_POST[ethnicity]','$_POST[gender]','$_POST[password]','$_POST[user_type]')";
-	//$sql2 = "INSERT INTO contacts (house_number, street_name, suburb, post_code, contact_number, id, user_type) VALUES ('$_POST[house_number]','$_POST[street_name]','$_POST[suburb]','$_POST[post_code]','$_POST[contact_number]','$_POST[id]','$_POST[user_type]')";
 
-if(mysqli_query($link, $sql1))
+	$msg = "";
+
+	if (isset($_POST['submit'])) {
+		$con = new mysqli('localhost', 'root', '', 'academicsTracker2');
+
+		$name = $con->real_escape_string($_POST['teacher_first_name']);
+		$mname = $con->real_escape_string($_POST['teacher_middle_name']);
+                $lname = $con->real_escape_string($_POST['teacher_last_name']);
+                $dob = $con->real_escape_string($_POST['date_of_birth']);
+                $race = $con->real_escape_string($_POST['ethnicity']);
+                $gen = $con->real_escape_string($_POST['gender']);
+				$type = $con->real_escape_string($_POST['user_type']);
+				$scode = $con->real_escape_string($_POST['school_code']);
+                $password = $con->real_escape_string($_POST['password']);
+		$cPassword = $con->real_escape_string($_POST['cPassword']);
+
+		if ($password != $cPassword)
+			$msg = "Please Check Your Passwords!";
+		else {
+			$hash = password_hash($password, PASSWORD_BCRYPT);
+			$con->query("INSERT INTO teacher (teacher_first_name,teacher_middle_name,teacher_last_name,date_of_birth,ethnicity,gender,user_type, school_code, password) VALUES ('$name', '$mname','$lname','$dob','$race','$gen','$type', '$scode', '$hash')");
+			readfile("successfulRecord3.html");// echo "Records Inserted Successfully.";
+		}
+	}
+
+
+ 
+if(mysqli_query($con, $sql1))
 {
 	//if(mysqli_query($link, $sql2))
 	//{   
@@ -28,9 +68,10 @@ if(mysqli_query($link, $sql1))
 }
  else
 {
-    echo "ERROR: Could Not Able To Execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could Not Able To Execute $sql. " . mysqli_error($con);
 }
 // Close connection
-mysqli_close($link);
+mysqli_close($con);
 
 ?>
+
