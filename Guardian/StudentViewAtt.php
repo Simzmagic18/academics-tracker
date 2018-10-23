22bc08a4161f9d6653ea938fb9839d4d14cca6d7
Simbarashe Sibusiso Sithole <simzmagic8@gmail.com>
2018-10-23 11:00:35 +0200

    guardian

--- Guardian/StudentViewAtt.php
+++ Guardian/StudentViewAtt.php
@@ -3,7 +3,7 @@
 <?php
 
 include('conn.php');//Database Connection
-include('session.php');
+//include('session.php');
 //session_start();
 
 	
@@ -107,82 +107,69 @@ include('session.php');
 
 <div class="super_container">
 
-	<!-- Header -->
+ <!-- Header -->
+ <header class="header d-flex flex-row">
+            <div class="header_content d-flex flex-row align-items-center">
+                    <!-- Logo -->
+                    <div class="logo_container"><a href="Parent_Guardian.html">
+                            <div class="logo">
+                                <img src="images/ATlogo221.png" alt="logo" width="64px" height="66px">&nbsp;
+                                <span>AT: Guardian</span>
+                            </div></a>
+                    </div>
 
-	<header class="header d-flex flex-row">
-		<div class="header_content d-flex flex-row align-items-center">
-			<!-- Logo -->
-			<div class="logo_container">
-				<div class="logo">
-					<img src="images/logo.png" alt="">
-					<span>course</span>
-				</div>
-			</div>
-
-			<!-- Main Navigation -->
-			<nav class="main_nav_container">
-				<div class="main_nav">
-					<ul class="main_nav_list">
-						<li class="main_nav_item"><a href="index.html">home</a></li>
-						<li class="main_nav_item"><a href="#">about us</a></li>
-						<li class="main_nav_item"><a href="courses.html">courses</a></li>
-						<li class="main_nav_item"><a href="elements.html">elements</a></li>
-						<li class="main_nav_item"><a href="news.html">news</a></li>
-						<li class="main_nav_item"><a href="#">contact</a></li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-		<div class="header_side d-flex flex-row justify-content-center align-items-center">
-			
-			<span>Administrator</span>
-		</div>
-
-		<!-- Hamburger -->
-		<div class="hamburger_container">
-			<i class="fas fa-bars trans_200"></i>
-		</div>
-
-	</header>
-	
-	<!-- Menu -->
-	<div class="menu_container menu_mm">
-
-		<!-- Menu Close Button -->
-		<div class="menu_close_container">
-			<div class="menu_close"></div>
-		</div>
-
-		<!-- Menu Items -->
-		<div class="menu_inner menu_mm">
-			<div class="menu menu_mm">
-				<ul class="menu_list menu_mm">
-					<li class="menu_item menu_mm"><a href="index.html">Home</a></li>
-					<li class="menu_item menu_mm"><a href="#">About us</a></li>
-					<li class="menu_item menu_mm"><a href="courses.html">Courses</a></li>
-					<li class="menu_item menu_mm"><a href="elements.html">Elements</a></li>
-					<li class="menu_item menu_mm"><a href="news.html">News</a></li>
-					<li class="menu_item menu_mm"><a href="#">Contact</a></li>
-				</ul>
-
-				<!-- Menu Social -->
-				
-				<div class="menu_social_container menu_mm">
-					<ul class="menu_social menu_mm">
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-instagram"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-twitter"></i></a></li>
-					</ul>
-				</div>
-
-				<div class="menu_copyright menu_mm">Colorlib All rights reserved</div>
-			</div>
-
-		</div>
-
+                    <!-- Main Navigation -->
+                    <nav class="main_nav_container">
+                            <div class="main_nav">
+                                    <ul class="main_nav_list">
+				    	<li class="main_nav_item"><a href="Guardian.html">HOME</a></li>
+					<li class="main_nav_item"><a href="../about.html">ABOUT US</a></li>
+					<li class="main_nav_item"><a href="../contact.html">CONTACT US</a></li>
+					<li class="main_nav_item"><a href="profile.html">PROFILE</a></li>
+					<li class="main_nav_item"><a href="logout.php">LOGOUT</a></li>
+                                    </ul>
+                            </div>
+                    </nav>
+            </div>
+        
+        <div class="header_side d-flex flex-row justify-content-center align-items-center"
+     		<ul class="nav navbar-nav navbar-right">
+      			<li class="dropdown dropdown-toggle"><a href="#" data-toggle="dropdown">
+	       			<span class="label label-pill label-danger count" style="border-radius:15px;"></span> 
+	       			<span class="glyphicon glyphicon-bell" style="font-size:18px;"></span>
+				</a>
+       		<ul class="dropdown-menu"></ul></li></ul>
 	</div>
+            
+
+            <!-- Hamburger -->
+            <div class="hamburger_container">
+                    <i class="fas fa-bars trans_200"></i>
+            </div>
+            </header>
+
+    <!-- Menu -->
+    <div class="menu_container menu_mm">
+
+            <!-- Menu Close Button -->
+            <div class="menu_close_container">
+                    <div class="menu_close"></div>
+            </div>
+
+            <!-- Menu Items -->
+            <div class="menu_inner menu_mm">
+                <div class="menu menu_mm">
+                    <ul class="menu_list menu_mm">
+                        <li class="menu_item menu_mm"><a href="Guardian.html">HOME</a></li>
+			<li class="menu_item menu_mm"><a href="../about.html">ABOUT US</a></li>
+			<li class="menu_item menu_mm"><a href="../contact.html">CONTACT US</a></li>					
+			<li class="menu_item menu_mm"><a href="profile.html">PROFILE</a></li>
+			<li class="menu_item menu_mm"><a href="logout.php">LOGOUT</a></li>
+                    </ul>
+                </div>
+            </div>
+    </div>
+
 	
 	<!-- Home -->
 
