3ada7b74a0d9355e68c965edd4d2a425421b8cf5
Simbarashe Sibusiso Sithole <simzmagic8@gmail.com>
2018-10-23 12:09:43 +0200

    Update Guardian/StudentViewAtt.php

--- Guardian/StudentViewAtt.php
+++ Guardian/StudentViewAtt.php
@@ -106,89 +106,76 @@ include ('session.php');
 
 <div class="super_container">
 
-	<!-- Header -->
+ <!-- Header -->
+ <header class="header d-flex flex-row">
+            <div class="header_content d-flex flex-row align-items-center">
+                    <!-- Logo -->
+                    <div class="logo_container"><a href="Parent_Guardian.html">
+                            <div class="logo">
+                                <img src="images/ATlogo221.png" alt="logo" width="64px" height="66px">&nbsp;
+                                <span>AT: Guardian</span>
+                            </div></a>
+                    </div>
 
-	<header class="header d-flex flex-row">
-		<div class="header_content d-flex flex-row align-items-center">
-			<!-- Logo -->
-			<div class="logo_container">
-				<div class="logo">
-					<img src="images/logo.png" alt="">
-					<span>course</span>
-				</div>
-			</div>
-
-			<!-- Main Navigation -->
-			<nav class="main_nav_container">
-				<div class="main_nav">
-					<ul class="main_nav_list">
-						<li class="main_nav_item"><a href="index.html">home</a></li>
-						<li class="main_nav_item"><a href="../about">about us</a></li>
-						<li class="main_nav_item"><a href="../contact.html">contact</a></li>
-			    	    <li class="main_nav_item"><a href="logout.php">logout</a></li>
-					
-					</ul>
-				</div>
-			</nav>
-		</div>
-		<div class="header_side d-flex flex-row justify-content-center align-items-center">
-			
-			<span>Administrator</span>
-		</div>
-
-		<!-- Hamburger -->
-		<div class="hamburger_container">
-			<i class="fas fa-bars trans_200"></i>
-		</div>
-
-	</header>
-	
-	<!-- Menu -->
-	<div class="menu_container menu_mm">
-
-		<!-- Menu Close Button -->
-		<div class="menu_close_container">
-			<div class="menu_close"></div>
-		</div>
-
-		<!-- Menu Items -->
-		<div class="menu_inner menu_mm">
-			<div class="menu menu_mm">
-				<ul class="menu_list menu_mm">
-					<li class="menu_item menu_mm"><a href="../index.html">Home</a></li>
-					<li class="menu_item menu_mm"><a href="../about.html">About us</a></li>
-					<li class="menu_item menu_mm"><a href="../contact.html#">Contact</a></li>
-			    	<li class="menu_item menu_mm"><a href="../logout.php">Contact</a></li>
-					
-				</ul>
-
-				<!-- Menu Social -->
-				
-				<div class="menu_social_container menu_mm">
-					<ul class="menu_social menu_mm">
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-instagram"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="menu_social_item menu_mm"><a href="#"><i class="fab fa-twitter"></i></a></li>
-					</ul>
-				</div>
-
-				<div class="menu_copyright menu_mm">Colorlib All rights reserved</div>
-			</div>
-
-		</div>
-
+                    <!-- Main Navigation -->
+                    <nav class="main_nav_container">
+                            <div class="main_nav">
+                                    <ul class="main_nav_list">
+				    	<li class="main_nav_item"><a href="Guardian.html">HOME</a></li>
+					<li class="main_nav_item"><a href="../about.html">ABOUT US</a></li>
+					<li class="main_nav_item"><a href="../contact.html">CONTACT US</a></li>
+					<li class="main_nav_item"><a href="profile.html">PROFILE</a></li>
+					<li class="main_nav_item"><a href="logout.php">LOGOUT</a></li>
+                                    </ul>
+                            </div>
+                    </nav>
+            </div>
+        
+        <div class="header_side d-flex flex-row justify-content-center align-items-center"
+     		<ul class="nav navbar-nav navbar-right">
+      			<li class="dropdown dropdown-toggle"><a href="#" data-toggle="dropdown">
+	       			<span class="label label-pill label-danger count" style="border-radius:15px;"></span> 
+	       			<span class="glyphicon glyphicon-bell" style="font-size:18px;"></span>
+				</a>
+       		<ul class="dropdown-menu"></ul></li></ul>
 	</div>
-	
-	<!-- Home -->
+            
+
+            <!-- Hamburger -->
+            <div class="hamburger_container">
+                    <i class="fas fa-bars trans_200"></i>
+            </div>
+            </header>
+
+    <!-- Menu -->
+    <div class="menu_container menu_mm">
+
+            <!-- Menu Close Button -->
+            <div class="menu_close_container">
+                    <div class="menu_close"></div>
+            </div>
+
+            <!-- Menu Items -->
+            <div class="menu_inner menu_mm">
+                <div class="menu menu_mm">
+                    <ul class="menu_list menu_mm">
+                        <li class="menu_item menu_mm"><a href="Guardian.html">HOME</a></li>
+			<li class="menu_item menu_mm"><a href="../about.html">ABOUT US</a></li>
+			<li class="menu_item menu_mm"><a href="../contact.html">CONTACT US</a></li>					
+			<li class="menu_item menu_mm"><a href="profile.html">PROFILE</a></li>
+			<li class="menu_item menu_mm"><a href="logout.php">LOGOUT</a></li>
+                    </ul>
+                </div>
+            </div>
+    </div>
+
 
 	<div class="home">
 		<div class="home_background_container prlx_parent">
 			<div class="home_background prlx" style="background-image:url(images/contact_background.jpg)"></div>
 		</div>
 		<div class="home_content">
-			<h6>Welcome<?php echo $row['student_id'];?> </h6>
+			<h6>Welcome: <?php echo $row['student_id'];?> </h6>
                         
 		</div>
 	</div>
@@ -246,111 +233,8 @@ include ('session.php');
 
 	<!-- Footer -->
 
-	<footer class="footer">
-		<div class="container">
-
-			<!-- Footer Content -->
-
-			<div class="footer_content">
-				<div class="row">
-
-					<!-- Footer Column - About -->
-					<div class="col-lg-3 footer_col">
-
-						<!-- Logo -->
-						<div class="logo_container">
-							<div class="logo">
-								<img src="images/logo.png" alt="">
-								<span>course</span>
-							</div>
-						</div>
-
-						<p class="footer_about_text">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum, tempor lacus.</p>
-
-					</div>
-
-					<!-- Footer Column - Menu -->
-
-					<div class="col-lg-3 footer_col">
-						<div class="footer_column_title">Menu</div>
-						<div class="footer_column_content">
-							<ul>
-								<li class="footer_list_item"><a href="index.html">Home</a></li>
-								<li class="footer_list_item"><a href="#">About Us</a></li>
-								<li class="footer_list_item"><a href="courses.html">Courses</a></li>
-								<li class="footer_list_item"><a href="news.html">News</a></li>
-								<li class="footer_list_item"><a href="#">Contact</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<!-- Footer Column - Usefull Links -->
-
-					<div class="col-lg-3 footer_col">
-						<div class="footer_column_title">Usefull Links</div>
-						<div class="footer_column_content">
-							<ul>
-								<li class="footer_list_item"><a href="#">Testimonials</a></li>
-								<li class="footer_list_item"><a href="#">FAQ</a></li>
-								<li class="footer_list_item"><a href="#">Community</a></li>
-								<li class="footer_list_item"><a href="#">Campus Pictures</a></li>
-								<li class="footer_list_item"><a href="#">Tuitions</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<!-- Footer Column - Contact -->
-
-					<div class="col-lg-3 footer_col">
-						<div class="footer_column_title">Contact</div>
-						<div class="footer_column_content">
-							<ul>
-								<li class="footer_contact_item">
-									<div class="footer_contact_icon">
-										<img src="images/placeholder.svg" alt="https://www.flaticon.com/authors/lucy-g">
-									</div>
-									Blvd Libertad, 34 m05200 Arévalo
-								</li>
-								<li class="footer_contact_item">
-									<div class="footer_contact_icon">
-										<img src="images/smartphone.svg" alt="https://www.flaticon.com/authors/lucy-g">
-									</div>
-									0034 37483 2445 322
-								</li>
-								<li class="footer_contact_item">
-									<div class="footer_contact_icon">
-										<img src="images/envelope.svg" alt="https://www.flaticon.com/authors/lucy-g">
-									</div>hello@company.com
-								</li>
-							</ul>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-			<!-- Footer Copyright -->
-
-			<div class="footer_bar d-flex flex-column flex-sm-row align-items-center">
-				<div class="footer_copyright">
-					<span><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span>
-				</div>
-				<div class="footer_social ml-sm-auto">
-					<ul class="menu_social">
-						<li class="menu_social_item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="menu_social_item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-					</ul>
-				</div>
-			</div>
-
-		</div>
-	</footer>
-
+	<div class="footer">
+	<Span>Academics Tracker All rights reserved</Span>
 </div>
 
 <script src="js/jquery-3.2.1.min.js"></script>
