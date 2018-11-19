d644ea888fc31d95b4f963a8898f543926def9ad
slzon1@student.monash.edu <slzon1@student.monash.edu>
2018-11-19 02:09:01 +0200

    update graph

--- Teacher/js/graph1.js
+++ Teacher/js/graph1.js
@@ -7,9 +7,40 @@ window.onload = function () {
         data:{
             labels:['Assignment 1', 'Exercise 1'],
             datasets:[{
-                label: 'Individual Marks %',
+                label: 'Peter Griffin',
                 data:[85, 100],
                 backgroundColor:'gold',
+                hoverBorderWidth:1,
+                hoverBorderColor:'black'
+            },
+            {
+                label: 'Lois Daniels',
+                data:[100, 95],
+                backgroundColor:'orange',
+                hoverBorderWidth:1,
+                hoverBorderColor:'black'
+            }],
+        },
+        options:{}
+    });
+
+    let chart2 = document.getElementById('chart2').getContext('2d');
+
+    let barChart1 = new Chart(chart2,{
+        type:'bar',
+        data:{
+            labels:['Assignment 1', 'Exercise 1'],
+            datasets:[{
+                label: 'Peter Griffin',
+                data:[60, 89],
+                backgroundColor:'gold',
+                hoverBorderWidth:1,
+                hoverBorderColor:'black'
+            },
+            {
+                label: 'Lois Daniels',
+                data:[70, 80],
+                backgroundColor:'orange',
                 hoverBorderWidth:1,
                 hoverBorderColor:'black'
             }],
